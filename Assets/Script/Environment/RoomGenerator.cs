2217e3b433710294089f45273fd4150e9ff84b9e
Uirochan <haumuchan@gmail.com>
2024-01-29 19:47:39 +0900

    stage gen2

--- Assets/Script/Environment/RoomGenerator.cs
+++ Assets/Script/Environment/RoomGenerator.cs
@@ -9,9 +9,39 @@ public class RoomGenerator : MonoBehaviour
     [SerializeField] private int z_start = -5;
 
     [Tooltip("length of sizes")]
-    [SerializeField] private int x_legth = 10;
-    [SerializeField] private int y_legth = 10;
-    [SerializeField] private int z_legth = 10;
+    [SerializeField] private int x_length = 10;
+    [SerializeField] private int y_length = 10;
+    [SerializeField] private int z_length = 10;
+
+    //generate one cube per call
+    private void GenerateCube(int x, int y, int z)
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        Material material = cube.GetComponent<Renderer>().material;
+        material.color = Color.gray;
+
+        cube.transform.position = new Vector3(x, y, z);
+        cube.transform.SetParent(transform, false);
+    }
+
+    private void GenerateRoom()
+    {   
+        for(int x = 0; x < x_length; x++)
+        {
+            for(int y = 0; y < y_length; y++)
+            {
+                for(int z  = 0; z < z_length; z++)
+                {
+                    GenerateCube(x + x_start, y + y_start, z + z_start);
+                }
+            }
+        }
+    }
+    private void Awake()
+    {
+        
+    }
 
 
 }
