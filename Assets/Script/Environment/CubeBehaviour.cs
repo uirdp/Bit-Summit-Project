4fe075d0252c9469ff47bd9b3fba4b4cdeda56f8
Uirochan <haumuchan@gmail.com>
2024-01-31 09:37:27 +0900

    bav

---
+++ Assets/Script/Environment/CubeBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    public int groupNumber = 0;
+
+    public Material normalMaterial;
+    public Material dangerousMaterial;
+}
