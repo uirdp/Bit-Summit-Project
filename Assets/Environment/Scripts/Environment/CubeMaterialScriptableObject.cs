33d8e5e2e6ace161e77cc75984ab1896959e3a10
Uirochan <haumuchan@gmail.com>
2024-01-31 10:06:06 +0900

    scriptable

---
+++ Assets/Environment/Scripts/Environment/CubeMaterialScriptableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CubeMaterialScriptableObject : ScriptableObject
+{
+    public Material defaultMaterial;
+    public Material dangerousMaterial;
+}
