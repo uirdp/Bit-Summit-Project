4c4f2542d32d77278349ef8d67e6874e8dd59b17
Uirochan <haumuchan@gmail.com>
2024-01-31 18:49:48 +0900

    it do be working

--- Assets/Environment/Scripts/Environment/CubeSignalManager.cs
+++ Assets/Environment/Scripts/Environment/CubeSignalManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class CubeSignalManager : MonoBehaviour
 {
     public GameObject room = null;
+
+    public CubeMaterialScriptableObject materials = null;
     public List<GameObject> cubes = new List<GameObject>();
 
     private int _count = 0;
@@ -32,10 +35,22 @@ public class CubeSignalManager : MonoBehaviour
         else { Debug.Log("there is no room");  }
     }
 
-    /*+private IEnumerator SendSignal()
+    private IEnumerator SendSignal()
+    { 
+        
+        cubes[_count].GetComponent<Renderer>().material = materials?.dangerousMaterial; 
+        yield return new WaitForSeconds(0.5f);
+        cubes[_count].GetComponent<Renderer>().material = materials?.defaultMaterial;
+        
+        _count++;
+        if (_count == cubes.Count) _count = 0; 
+        StartCoroutine(SendSignal());
+    }
+
+    private void Start()
     {
-       
-    }*/
+        StartCoroutine(SendSignal());
+    }
     // Update is called once per frame
     void Update()
     {
