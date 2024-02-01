70ca747cace005ec7ff187baf1a0d971e51b61bb
Uirochan <haumuchan@gmail.com>
2024-02-01 20:09:15 +0900

    wip

--- Assets/Environment/Scripts/CubeColorMatrix.cs
+++ Assets/Environment/Scripts/CubeColorMatrix.cs
@@ -9,4 +9,22 @@ public class CubeColorMatrix : MonoBehaviour
     //static matrix should be inserted to colorMatrix
     //move left(), right(), up(), down() diag()...
     //Do I need a real matrix? think just inx is enough
+    //or perhaps, create a actual matrix and update 
+    //every element
+
+    private void GetColorMatrixFromCubeMatrixGenerator(int[,] mat)
+    {
+        colorMatrix = mat;
+    }
+
+    private void CreateColorMatrix(int rows, int columns)
+    {
+        for(int i = 0; i < rows; i++)
+        {
+            for( int j = 0; j < columns; j++)
+            {
+                colorMatrix[i, j] = 0;
+            }
+        }
+    }
 }
