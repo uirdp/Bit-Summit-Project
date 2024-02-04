1d3fd6352fa72904bc74ad383bac64a9ea39e836
Uirochan <haumuchan@gmail.com>
2024-02-04 10:38:39 +0900

    deleted unused part

--- Assets/Environment/Scripts/CubeSignalManager.cs
+++ Assets/Environment/Scripts/CubeSignalManager.cs
@@ -25,51 +25,7 @@ public class CubeSignalManager : MonoBehaviour
         cubeMatrix = matrices?.cubeMatrix;
         if(cubeMatrix == null) GetCubeMatrix();
     }
-
-    //should be renamed to ChangeMaterial
-    private IEnumerator SendSignal()
-    { 
-        
-        cubes[_count].GetComponent<Renderer>().material = materials?.dangerousMaterial; 
-        yield return new WaitForSeconds(0.5f);
-        cubes[_count].GetComponent<Renderer>().material = materials?.defaultMaterial;
-        
-        _count++;
-        if (_count == cubes.Count) _count = 0; 
-        StartCoroutine(SendSignal());
-    }
-
-    //not sure if I am gonna need this
-    //changes color by group number, which is assign to cubes individually
-    //under construction
-    private IEnumerator SwitchCubeMaterialsWithGroupNumber(int groupNum)
-    {
-        yield return null;
-    }
-
-    //use color colour matrix's index to switch colors
-    private IEnumerator SwitchCubeMaterialsWithColorMatrix(int x, int z, int xsz, int zsz)
-    {
-        for(int i = 0; i < xsz; i++)
-        {
-            for(int j = 0; z < zsz; z++)
-            {
-                cubeMatrix[x + i, z + j].GetComponent<Renderer>().material = materials?.dangerousMaterial;
-            }
-        }
-
-        yield return new WaitForSeconds(0.5f);
-
-        for (int i = 0; i < xsz; i++)
-        {
-            for (int j = 0; z < zsz; z++)
-            {
-                cubeMatrix[x + i, z + j].GetComponent<Renderer>().material = materials?.dangerousMaterial;
-            }
-        }
-    }
-
-    //TODO: make materials enum or array?, so that materials?.materials(colorMat[i, j])
+  
     public void ChangeCubeMaterials(ref int[,] colorMatrix)
     {
         for(int i = 0; i < colorMatrix.GetLength(0); i++)
