d78c55e8d4e05909d36b8372961f1ab5c2f6db90
Uirochan <haumuchan@gmail.com>
2024-02-06 11:22:04 +0900

    comments

--- Assets/Environment/Scripts/Matrices/Color Matrices/ColorMatrixInstance.cs
+++ Assets/Environment/Scripts/Matrices/Color Matrices/ColorMatrixInstance.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//This class instantiate a colorMatrix to manipulate
 public class ColorMatrixInstance : MonoBehaviour
 {
     [Tooltip("Name of the matrix you are intending to use")]
@@ -14,6 +15,8 @@ public class ColorMatrixInstance : MonoBehaviour
     {
         dict = new MatrixDictionary();
         colorMatrix = dict.ReturnMatrix(matrixName);
+
+        if (colorMatrix == null) Debug.Log("unable to fetch matrix");
     }
 
     private void Awake()
