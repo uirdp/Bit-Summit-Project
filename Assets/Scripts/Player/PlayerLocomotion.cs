d99b8632d1340afc91434b3d68561624f273832a
Uirochan <haumuchan@gmail.com>
2024-02-16 22:13:35 +0900

    what be a clear code for locomotive?

---
+++ Assets/Scripts/Player/PlayerLocomotion.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    public float rotSpeed;
+    public float moveSpeed;
+}
