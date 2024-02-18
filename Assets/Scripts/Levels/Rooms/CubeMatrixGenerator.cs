8d75908ea38bda690aec14c017206e9102911279
Uirochan <haumuchan@gmail.com>
2024-02-18 20:32:11 +0900

    adding key exp

---
+++ Assets/Scripts/Levels/Rooms/CubeMatrixGenerator.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Should Be renamed to cubematrix
+public class CubeMatrixGenerator : MonoBehaviour
+{
+    public GameObject room = null;
+
+    public MaterialList materials = null;
+    public List<GameObject> cubes = new List<GameObject>();
+
+    public int rows;
+    public int columns;
+
+    public GameObject[,] cubeMatrix = null;
+    public int[,] colorMatrix = null;
+
+    [ContextMenu("Make List")]
+    private void MakeList()
+    {
+        if (room != null)
+        {
+            if (cubes?.Count == 0)
+            {
+                foreach (Transform child in room.transform)
+                {
+                    cubes.Add(child.gameObject);
+                }
+            }
+
+            else
+            {
+                Debug.Log("Cube list already exist");
+            }
+        }
+
+        else { Debug.Log("there is no room"); }
+    }
+
+    private void CreateMatrixFromGroupNumber()
+    {
+
+    }
+
+    [ContextMenu("Create Matrices With Position")]
+    private void CreateMatricesWithPosition()
+    {
+        cubeMatrix = new GameObject[rows, columns];
+        colorMatrix = new int[rows, columns];
+
+        int ind = 0;
+        if (room != null)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    cubeMatrix[i, j] = room.transform.GetChild(ind).gameObject;
+                    ind++;
+                }
+            }
+        }
+
+        else { Debug.Log("there is no room"); }
+    }
+
+    [ContextMenu("CheckMatrix")]
+    private void CheckMatrix()
+    {
+        if (cubeMatrix != null)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Debug.Log("(" + i + "," + j + ") = " + cubeMatrix[i, j]);
+                }
+            }
+        }
+
+        else Debug.Log("there is no matrix");
+    }
+
+    private void Awake()
+    {
+        CreateMatricesWithPosition();
+    }
+
+}
---
+++ Assets/Scripts/Levels/Rooms/CubeSignalManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class CubeSignalManager : MonoBehaviour
+{
+    public GameObject room = null;
+
+    public MaterialList materials = null;
+    public MaterialList materialList = null;
+
+    public CubeMatrixGenerator matrices = null;
+
+    public List<GameObject> cubes = new List<GameObject>();
+    public GameObject[,] cubeMatrix;
+
+    //Do you need a copy though?
+    //if the matrix is refered by some more objects, 
+    //then I will not need a copy
+    private void GetCubeMatrix()
+    {
+        cubeMatrix = matrices?.cubeMatrix;
+        if(cubeMatrix == null) GetCubeMatrix();
+    }
+  
+    //rewrite all cubes
+    public void ChangeCubeMaterials(ref int[,] colorMatrix)
+    {
+        for(int i = 0; i < colorMatrix.GetLength(0); i++)
+        {
+            for( int j = 0; j < colorMatrix.GetLength(1); j++)
+            {
+                cubeMatrix[i, j].GetComponent<Renderer>().material 
+                        = materialList.materials[colorMatrix[i,j]];
+
+                cubeMatrix[i, j].gameObject.tag = materialList.statusList[colorMatrix[i,j]];
+            }
+        }
+    }
+
+    private void Start()
+    {
+        GetCubeMatrix();
+    }
+}
