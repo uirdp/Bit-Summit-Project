9d554f8549a29947e8ee68a36645bbfe1d70d278
Uirochan <haumuchan@gmail.com>
2024-02-19 15:36:58 +0900

    IMatModel改訂

--- Assets/Scripts/Matrix/Models/IMatrixModel.cs
+++ Assets/Scripts/Matrix/Models/IMatrixModel.cs
@@ -1,21 +1,31 @@
 using static DirectionSpace.Directions;
 using System.Collections.Generic;
+using System.Numerics;
 
 //All static matrices should inherit this Interface
 
 //MatrixInstant(Instance of a static matrix) should look for a dictionary
 //through this Interface
+
+public struct Area
+{
+    public Area(Vector2 pos, Vector2 size, Direction[] man)
+    {
+        Position = pos;
+        Size = size;
+        Manual = man;
+    }
+
+    public Vector2 Position { get; set; }
+    public Vector2 Size { get; set; }
+    public Direction[] Manual { get; }
+
+}
 public interface IMatrixModel
 {
     public int[,] InitMatrix { get; }
     public ref int[,] Matrix { get; }
-    public List<int[,]> MovingMatrices { get; }
-
-    ref Point[] PosOfMovingMatrices { get; }
-
-    ref Point[] SizeOfMovingMatrices { get; }
-
-    public Direction[] Directions { get; }
-
+    public Area[] RedAreas { get; }
+    public Area[] GreenAreas { get; }
     
 }
