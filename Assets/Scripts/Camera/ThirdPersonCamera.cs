8de9a17e09171019b5ea407af5b7704daa25940e
Uirochan <haumuchan@gmail.com>
2024-02-23 19:58:11 +0900

    camera

--- Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VSX.UniversalVehicleCombat;
+using Cinemachine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameAgent player;
 
-    // Update is called once per frame
-    void Update()
+    private void StartFollowing(Vehicle player)
     {
-        
+        var followTarget = player.transform.GetChild(0);
+
+        var vcam = GetComponent<CinemachineVirtualCamera>();
+        vcam.Follow = followTarget;
     }
 }
