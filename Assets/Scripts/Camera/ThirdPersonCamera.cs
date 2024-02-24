566a48340729dae0756af48f4a8e51c3ebdede43
Uirochan <haumuchan@gmail.com>
2024-02-24 19:27:42 +0900

    private -> public

--- Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -7,7 +7,7 @@ using Cinemachine;
 public class ThirdPersonCamera : MonoBehaviour
 {
     //‚±‚ê‚ğƒJƒƒ‰‚É‚Â‚¯‚ÄALoadOut-‚©‚ç“Ç‚ñ‚Å‚­‚¾‚³‚¢
-    private void StartFollowing(Vehicle target)
+    public void StartFollowing(Vehicle target)
     {
         var followTarget = target.transform.GetChild(0); //camera root
 
