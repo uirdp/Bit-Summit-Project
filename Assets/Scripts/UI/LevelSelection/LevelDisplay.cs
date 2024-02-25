2f548de87c931df06fd1143f26e00addee615126
Uirochan <haumuchan@gmail.com>
2024-02-25 17:26:10 +0900

    modefied level display

--- Assets/Scripts/UI/LevelSelection/LevelDisplay.cs
+++ Assets/Scripts/UI/LevelSelection/LevelDisplay.cs
@@ -7,9 +7,36 @@ public class LevelDisplay : MonoBehaviour
     public GameObject[] Levels;
     private int index = 0;
 
+    public int Index => index;
     public void DisplayNextLevel()
     {
         Levels[index++].SetActive(false);
+
+        CheckIndex();
+
         Levels[index].SetActive(true);
     }
+
+    public void DisplayPreviousLevel()
+    {
+        Levels[index--].SetActive(false);
+
+        CheckIndex();
+
+        Levels[index].SetActive(true);
+    }
+
+    private void CheckIndex()
+    {
+        if (index >= Levels.Length)
+        {
+            index = 0;
+        }
+
+        if (index < 0)
+        {
+            index = Levels.Length - 1;
+        }
+    }
+
 }
