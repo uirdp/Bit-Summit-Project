bc50c67ea73643d3a582779a4e981f587c5f1970
Uirochan <haumuchan@gmail.com>
2024-02-28 12:17:35 +0900

    LoadLevelComplete

--- Assets/Scripts/UI/LevelLoader.cs
+++ Assets/Scripts/UI/LevelLoader.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Michsky.UI.Heat;
 
 public class LevelLoader : MonoBehaviour
 {
+    public ChapterManager chapterManager;
+
+    public void LoadLevel()
+    {
+        chapterManager.chapters[chapterManager.currentChapterIndex].onPlay.Invoke();
+    }
     public void LoadGardenLevel()
     {
         SceneManager.LoadScene("GardenScene");
